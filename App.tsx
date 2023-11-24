55f7292ff267a19e28592cd8116de88aa1fab179
Mike de Geofroy <mikedegeofroy@gmail.com>
2023-11-24 22:46:39 +0300

    Added map

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from "react-native-maps";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <MapView
+          style={styles.map}
+          showsCompass={false}
+          region={{
+            latitude: 30.3609,
+            longitude: 59.9311,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          showsPointsOfInterest={false}
+        >
+
     </View>
   );
 }
@@ -16,5 +29,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
