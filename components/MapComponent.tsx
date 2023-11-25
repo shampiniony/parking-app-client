f76ef7680ed5004c1631c44274f26f116cc9d6eb
Mike de Geofroy <mikedegeofroy@gmail.com>
2023-11-25 22:46:39 +0300

    Removed map component

--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -5,14 +5,10 @@ import ParkingLot from "./ParkingLot";
 import React from 'react';
 import { useParkingData } from "../hooks/useParkingData";
 import { MapContext } from "../context/MapContext";
-import MapViewDirections from 'react-native-maps-directions';
 
 export default function MapComponent() {
   const parkings = useParkingData();
   const mapViewRef = useContext(MapContext);
-
-  const origin = {latitude: 37.3318456, longitude: -122.0296002};
-  const destination = {latitude: 37.771707, longitude: -122.4053769};
 
   return (
     <MapView
@@ -27,11 +23,6 @@ export default function MapComponent() {
       showsCompass={false}
       showsPointsOfInterest={false}
     >
-      <MapViewDirections
-        origin={origin}
-        destination={destination}
-        apikey={'AIzaSyCx3n09Zpghadz7-CH2BP3wW-yq9UcH5M8'}
-      />
       {parkings?.map((spot, index) => (
         <ParkingLot key={index} spot={spot} />
       ))}
