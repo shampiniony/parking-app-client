4754b9da7209d5f1b477e615fc7be78c0766601b
Mike de Geofroy <mikedegeofroy@gmail.com>
2023-11-26 00:30:24 +0300

    Aahhh kill me

--- App.tsx
+++ App.tsx
@@ -1,18 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import SearchBar from "./components/SearchBar";
 import MapComponent from "./components/MapComponent";
 import Drawer from "./components/Drawer";
 import { ParkingContext } from "./context/ParkingContext";
-import { useRef, useState } from "react";
-import MapView from 'react-native-maps';
-import { MapContext } from './context/MapContext';
-import { PersonalAccount } from './modules/personal-account/personal-account';
+import { useState } from "react";
 import { DrawerContext } from "./context/DrawerContext";
 import { Parking } from "./models/parkings";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
+  const [parking, setParking] = useState<Parking | null>(null);
   const parkingValue = { parking, setParking };
 
   const [extended, setExtended] = useState<boolean>(false);
