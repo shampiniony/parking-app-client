5db6a224154999c539d8c6ebce0b034339acb220
Mike de Geofroy <mikedegeofroy@gmail.com>
2023-11-26 00:24:04 +0300

    Fixed map context

--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -1,31 +1,45 @@
-import { useContext } from "react";
+import { useEffect, useRef, useState } from "react";
 import { StyleSheet } from 'react-native';
 import MapView from "react-native-maps";
+import { Parking } from "./../models/parkings";
 import ParkingLot from "./ParkingLot";
-import { useParkingData } from "../hooks/useParkingData";
 import { MapContext } from "../context/MapContext";
 
 export default function MapComponent() {
-  const parkings = useParkingData();
-  const mapViewRef = useContext(MapContext);
+  const [parkings, setParkings] = useState<Parking[]>();
+  const mapViewRef = useRef<MapView>(null);
+
+  const getParkingData = () => {
+    fetch('http://172.232.44.175/api/parkings?format=json')
+    .then(response => response.json())
+    .then((json) => {
+        setParkings(json);
+      })
+  }
+
+  useEffect(() => {
+    getParkingData();
+  }, [])
 
   return (
-    <MapView
-      ref={mapViewRef}
-      initialRegion={{
-        latitude: 56.8431,
-        longitude: 60.6454,
-        latitudeDelta: 0.09,
-        longitudeDelta: 0.09,
-      }}
-      style={styles.map}
-      showsCompass={false}
-      showsPointsOfInterest={false}
-    >
-      {parkings?.map((spot, index) => (
-        <ParkingLot key={index} spot={spot} />
-      ))}
-    </MapView>
+    <MapContext.Provider value={mapViewRef}>
+      <MapView
+        ref={mapViewRef}
+        initialRegion={{
+          latitude: 56.8431,
+          longitude: 60.6454,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.09,
+        }}
+        style={styles.map}
+        showsCompass={false}
+        showsPointsOfInterest={false}
+      >
+        {parkings?.map((spot, index) => (
+          <ParkingLot key={index} spot={spot} />
+        ))}
+      </MapView>
+    </MapContext.Provider>
   )
 }
 
